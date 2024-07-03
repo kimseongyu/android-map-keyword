fe87c960ee4b8c93362eda36a46e86d327a6c1da
sun <50648835+kimseongyu@users.noreply.github.com>
2024-07-03 22:44:01 +0900

    feat: update MainActivity
    - using ViewBinding for access xml easily
    - add ViewModel to interact models

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -2,10 +2,23 @@ package campus.tech.kakao.map
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import campus.tech.kakao.map.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: SearchViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val searchRepository = SearchRepository(this)
+        val viewModelProviderFactory = SearchViewModelFactory(searchRepository)
+        viewModel =
+            ViewModelProvider(this, viewModelProviderFactory).get(SearchViewModel::class.java)
     }
 }
