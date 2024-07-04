6a654075cde8053ec5e42a213ede7b5097f79fb7
sun <50648835+kimseongyu@users.noreply.github.com>
2024-07-05 08:42:51 +0900

    feat: update MainActivity
    - delSearchWord: when user push editText's button, input text is deleted
    - detectsearchWindowChanged: after the user enters the text,
    it is displayed if there is a search result
    - getSavedSearchWord: when saved search words  changed, recyclerview is changed
    - showView: decide view to see

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -1,15 +1,19 @@
 package campus.tech.kakao.map
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import campus.tech.kakao.map.databinding.ActivityMainBinding
-
-private lateinit var binding: ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: SearchViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +23,60 @@ class MainActivity : AppCompatActivity() {
         val searchRepository = SearchRepository(this)
         val viewModelProviderFactory = SearchViewModelFactory(searchRepository)
         viewModel =
-            ViewModelProvider(this, viewModelProviderFactory).get(SearchViewModel::class.java)
+            ViewModelProvider(this, viewModelProviderFactory)[SearchViewModel::class.java]
+
+        delSearchWord()
+        detectSearchWindowChanged()
+        getSavedSearchWord()
+    }
+
+    private fun delSearchWord() {
+        binding.delSearchWord.setOnClickListener {
+            binding.searchWindow.text = null
+        }
+    }
+
+    private fun detectSearchWindowChanged() {
+        binding.searchWindow.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val searchWord = SearchWord(s.toString())
+                viewModel.getSearchResults(searchWord)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+
+        lifecycleScope.launch {
+            viewModel.searchResults.collect {
+                if (it.isEmpty()) {
+                    showView(binding.searchResultEmpty, true)
+                    showView(binding.searchResultsList, false)
+                } else {
+                    showView(binding.searchResultEmpty, false)
+                    showView(binding.searchResultsList, true)
+                    binding.searchResultsList.adapter = SearchResultsAdapter(it, layoutInflater, viewModel::saveSearchWord)
+                    binding.searchResultsList.layoutManager = LinearLayoutManager(this@MainActivity)
+                }
+            }
+        }
+    }
+
+    private fun getSavedSearchWord() {
+        lifecycleScope.launch {
+            viewModel.savedSearchWords.collect{
+                binding.savedSearchWordsList.adapter = SavedSearchWordsAdapter(it, layoutInflater, viewModel::delSavedSearchWord)
+                binding.savedSearchWordsList.layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.HORIZONTAL, false)
+            }
+        }
+    }
+
+    private fun showView(view: View, isShow: Boolean) {
+        view.visibility = if (isShow) View.VISIBLE else View.GONE
     }
 }
