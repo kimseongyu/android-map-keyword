12a46c6739f654e3257d406da272360d3ec53f71
sun <50648835+kimseongyu@users.noreply.github.com>
2024-07-05 08:35:54 +0900

    feat: add adapter for check saved search word

---
+++ app/src/main/java/campus/tech/kakao/map/SavedSearchWordsAdapter.kt
@@ -0,0 +1,37 @@
+package campus.tech.kakao.map
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import campus.tech.kakao.map.databinding.SavedSearchWordItemBinding
+
+class SavedSearchWordsAdapter (
+    private val savedSearchWords: List<SearchWord>,
+    private val layoutInflater: LayoutInflater,
+    private val delSavedSearchWord: (SearchWord) -> Unit
+) :
+    RecyclerView.Adapter<SavedSearchWordsAdapter.ViewHolder>() {
+    inner class ViewHolder(binding: SavedSearchWordItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        val savedSearchWord: TextView = binding.SavedSearchWord
+        init{
+            binding.delSavedSearchWord.setOnClickListener {
+                val searchWord = SearchWord(savedSearchWord.text.toString())
+                delSavedSearchWord(searchWord)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = SavedSearchWordItemBinding.inflate(layoutInflater, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return savedSearchWords.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.savedSearchWord.text = savedSearchWords[position].searchword
+    }
+}
