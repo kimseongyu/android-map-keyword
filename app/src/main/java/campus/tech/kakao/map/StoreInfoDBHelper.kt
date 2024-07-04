41cf0a36e36371dca58b1efa380ac0e40e4f757a
sun <50648835+kimseongyu@users.noreply.github.com>
2024-07-05 08:24:10 +0900

    feat: add search word database

--- app/src/main/java/campus/tech/kakao/map/StoreInfoDBHelper.kt
+++ app/src/main/java/campus/tech/kakao/map/StoreInfoDBHelper.kt
@@ -5,28 +5,36 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.provider.BaseColumns
 
-private const val SQL_CREATE_ENTRIES =
+private const val SQL_STORE_INFO_CREATE_ENTRIES =
     "CREATE TABLE ${StoreInfoEntry.TABLE_NAME} (" +
             "${BaseColumns._ID} INTEGER PRIMARY KEY," +
             "${StoreInfoEntry.STORE_NAME} TEXT," +
             "${StoreInfoEntry.STORE_LOCATION} TEXT," +
             "${StoreInfoEntry.STORE_CATEGORY} TEXT)"
 
-private const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${StoreInfoEntry.TABLE_NAME}"
+private const val SQL_SEARCH_WORD_CREATE_ENTRIES =
+    "CREATE TABLE ${SearchWordEntry.TABLE_NAME} (" +
+            "${BaseColumns._ID} INTEGER PRIMARY KEY," +
+            "${SearchWordEntry.SEARCH_WORD} TEXT UNIQUE)"
+
+private const val SQL_STORE_INFO_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${StoreInfoEntry.TABLE_NAME}"
+private const val SQL_SEARCH_WORD_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${StoreInfoEntry.TABLE_NAME}"
 
 class StoreInfoDBHelper(context: Context) :
     SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     companion object {
         const val DATABASE_NAME = "StoreInfo.db"
-        const val DATABASE_VERSION = 1
+        const val DATABASE_VERSION = 2
     }
 
     override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL(SQL_CREATE_ENTRIES)
+        db.execSQL(SQL_STORE_INFO_CREATE_ENTRIES)
+        db.execSQL(SQL_SEARCH_WORD_CREATE_ENTRIES)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        db.execSQL(SQL_DELETE_ENTRIES)
+        db.execSQL(SQL_STORE_INFO_DELETE_ENTRIES)
+        db.execSQL(SQL_SEARCH_WORD_DELETE_ENTRIES)
         onCreate(db)
     }
 }
