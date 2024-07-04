e1252b0e37f4545b277137e3837f8b2ee0c3066c
sun <50648835+kimseongyu@users.noreply.github.com>
2024-07-05 08:34:00 +0900

    feat: add functions for searching

--- app/src/main/java/campus/tech/kakao/map/SearchViewModel.kt
+++ app/src/main/java/campus/tech/kakao/map/SearchViewModel.kt
@@ -1,13 +1,36 @@
 package campus.tech.kakao.map
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class SearchViewModel(private val searchRepository: SearchRepository) : ViewModel() {
+
+    private val _searchResults = MutableStateFlow<List<StoreInfo>>(emptyList())
+    val searchResults: StateFlow<List<StoreInfo>> get() = _searchResults
+    private val _savedSearchWords = MutableStateFlow<List<SearchWord>>(emptyList())
+    val savedSearchWords: StateFlow<List<SearchWord>> get() = _savedSearchWords
     init{
         setDefaultData()
+        getSavedSearchWords()
     }
 
     private fun setDefaultData() {
         searchRepository.setDefaultData()
+    }
+    fun getSearchResults(searchWord: SearchWord) {
+        _searchResults.value = searchRepository.getSearchResults(searchWord)
+    }
+    fun saveSearchWord(searchWord: SearchWord){
+        searchRepository.saveSearchWord(searchWord)
+        getSavedSearchWords()
+    }
+    fun getSavedSearchWords(){
+        _savedSearchWords.value = searchRepository.getSavedSearchWords()
+    }
+
+    fun delSavedSearchWord(searchWord: SearchWord){
+        searchRepository.delSavedSearchWord(searchWord)
+        getSavedSearchWords()
     }
 }
