577a17c2475f72a093fcbec29520006c2c53afc6
sun <50648835+kimseongyu@users.noreply.github.com>
2024-07-05 08:23:33 +0900

    refactor: save to same file

--- app/src/main/java/campus/tech/kakao/map/StoreInfo.kt
+++ app/src/main/java/campus/tech/kakao/map/StoreInfo.kt
@@ -1,7 +1,16 @@
 package campus.tech.kakao.map
 
+import android.provider.BaseColumns
+
 data class StoreInfo(
     val name: String,
     val location: String,
     val category: String
 )
+
+object StoreInfoEntry : BaseColumns {
+    const val TABLE_NAME = "storeInfo"
+    const val STORE_NAME = "name"
+    const val STORE_LOCATION = "location"
+    const val STORE_CATEGORY = "category"
+}
